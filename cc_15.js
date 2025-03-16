df3954619d60c4aaaef9bb96e60e4c8c1bec26d0
Alikhan <fixator2004@gmail.com>
2025-03-15 23:42:14 -0400

    Task 1 - Base Structure Setup

---
+++ cc_15.js
@@ -0,0 +1,4 @@
+//Task 1: Creating the Base Structure
+
+const selectDashboard = document.getElementById("riskDashboard");   //selecting the riskDashboard container in the HTML file
+console.log("Risk Dashboard Loaded");     //logging the messege to the console
