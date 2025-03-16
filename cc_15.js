a28256a05ebba9eb928aefcfe839426647af6cdd
Alikhan <fixator2004@gmail.com>
2025-03-16 15:41:17 -0400

    Task 5 - Bulk Risk Updates

--- cc_15.js
+++ cc_15.js
@@ -29,11 +29,11 @@ if(riskLevel === "Low"){
     riskItem.classList.add("high")
 }
 
+riskItem.setAttribute("risk-levelAttribute", riskLevel) //set an attribute for riskLevel variable to be used in task 5
 
-
-riskItem.innerHTML =                    //created a structure for the risk card using .innerHTML
+riskItem.innerHTML =                    //created a structure for the risk card using .innerHTML; assigned class to risk level
     `<h2>${riskName}</h2>
-    <p>Risk: ${riskLevel}</p>
+    <p class = "risk-level">Risk: ${riskLevel}</p>    
     <p>Department: ${department}</p>`   
 
     selectDashboard.appendChild(riskItem);
@@ -42,6 +42,7 @@ riskItem.innerHTML =                    //created a structure for the risk card 
 
     
 }
+//used preventDefault to prevent reloading the page when submit is clocked 
 selectForm.addEventListener("submit", (event) =>{
     event.preventDefault();
 
@@ -53,6 +54,39 @@ const department = document.getElementById("department").value;
 addRiskItem(riskName,riskLevel,department);     //running the function using test data
 
 });
+
+//Task 5: Implementing Bulk Updates
+const riskLvlUp = document.getElementById("increaseRiskLevels") 
+
+riskLvlUp.addEventListener("click", ()=>{                           //added an event listener that updates the risk level difficuly
+    document.querySelectorAll(".riskCard").forEach((riskItem) => {
+    let oldRiskLevel = riskItem.getAttribute("risk-levelAttribute")
+    let newRiskLevel = oldRiskLevel
+
+        if(oldRiskLevel === "Low")      //updating low and medium risk levels using if else statement
+            newRiskLevel = "Medium"
+        else if (oldRiskLevel === "Medium")
+            newRiskLevel = "High"
+
+        riskItem.setAttribute("risk-levelAttribute", newRiskLevel)
+
+        riskItem.classList.remove("low", "med", "high")     //removed old classes to apply new classes 
+
+            if (newRiskLevel === "Low")         //updating class item to apply styling to the risk cards 
+                riskItem.classList.add("low")
+            else if (newRiskLevel === "Medium")
+                riskItem.classList.add("med")
+            else if (newRiskLevel === "High")
+                riskItem.classList.add("high")
+
+            //selected risk level class using query selector and updated the new card text using .innerHTML
+            riskItem.querySelector(".risk-level").innerHTML = `Risk: ${newRiskLevel}`   
+        })
+
+    
+});
+
+
 //Test Case:
 addRiskItem("Data Breach", "High", "IT");
 addRiskItem("Supply Chain Disruption", "Medium", "Operations");
@@ -62,3 +96,6 @@ addRiskItem("Market Fluctuations", "High", "Finance");
 //Test Case:
 addRiskItem("Cybersecurity Threat", "High", "IT");
 addRiskItem("HR Compliance Issue", "Low", "Human Resources");
+//Test Case:
+addRiskItem("Employee Retention", "Low", "HR");
+// Clicking "Increase Risk Levels" should change it to "Medium".
